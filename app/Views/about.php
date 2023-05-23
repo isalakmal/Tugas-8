59634a9932a691ebfcc8bf2664e4a531b1b64049
isalakmal <93710502+isalakmal@users.noreply.github.com>
2023-05-23 11:04:34 +0700

    Add files via upload

---
+++ app/Views/about.php
@@ -0,0 +1,46 @@
+<?= $this->include('tamplate/header'); ?>
+
+<h1><?= $title; ?></h1>
+<hr>
+<p><?= $content; ?></p>
+<br>
+<div class="row">
+    
+    <br>
+    <center>
+        <h1>piero putra persada</h1>
+    </center>
+    <center>
+        <p>Nama saya piero putra persada, teman saya biasa memanggil saya EER saya juga seorang, mahasiswa Teknik Informatika di <strong>Universitas Pelita Bangsa.</strong>
+        <h3>Biodata Mahasiswa</h3>
+        <table>
+            <p>
+                <tr>
+                    <td>Kelas</td>
+                    <td>:</td>
+                    <td>TI.21.A.1</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </p>
+            <p>
+                <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td>faisal akmal firdaus</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </p>
+            <p>
+                <tr>
+                    <td>Nim</td>
+                    <td>:</td>
+                    <td>312110160</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </p>
+        </table>
+        </p>
+    </center>
+</div>
+
+<?= $this->include('tamplate/footer'); ?>
